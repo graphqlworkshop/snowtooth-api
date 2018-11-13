60f868535eeb8ab64a3226243e5ffe9fc1f2ce98
eveporcello <eve@moonhighway.com>
2018-11-13 07:28:04 -0800

    Adjustment to start files

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-const { ApolloServer } = require('apollo-server')
 const lifts = require('./data/lifts.json')
 const trails = require('./data/trails.json')
 
-const typeDefs = `
-    type Query {
-        liftCount: Int!
-    }
-`
-
-const resolvers = {
-  Query: {
-    liftCount: () => lifts.length
-  }
-}
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-})
-
-server.listen().then(({ url }) => {
-  console.log(`Server running at ${url}`)
-})
+console.log(`Build your GraphQL Server Here!`)
